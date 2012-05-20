aff285179ddebf8edbcb1dd7a464e86509891a94
etsuei <eric@townsquaredigitial.com>
2012-05-20 17:22:21 -0400

    google analytics added to head

--- wp-content/themes/hyc/header/header-default.php
+++ wp-content/themes/hyc/header/header-default.php
@@ -53,6 +53,19 @@ $use_background_img == 'no' ? $css_ext = '?type=noimg' : $css_ext = '';
 	<![endif]-->
 	
 	<?php wp_head(); ?>
+    <script type="text/javascript">
+
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-30972974-1']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+
+    </script>
 </head>
 
 <body <?php body_class(); ?>>
